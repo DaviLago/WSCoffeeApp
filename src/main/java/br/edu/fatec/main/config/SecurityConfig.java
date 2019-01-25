c75e3265c45ef4d9c9e516bf132f82c5305829f8
DaviLago <davilagoo@gmail.com>
2019-01-24 23:08:28 -0200

    Build

--- src/main/java/br/edu/fatec/main/config/SecurityConfig.java
+++ src/main/java/br/edu/fatec/main/config/SecurityConfig.java
@@ -91,7 +91,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	public PasswordEncoder passwordEncoder() { 
 		return new BCryptPasswordEncoder();
 	}
 
