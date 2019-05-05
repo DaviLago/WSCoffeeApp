87301281b8d3f0c73b848774ffea45962d1af536
DaviLago <davilagoo@gmail.com>
2019-05-05 13:05:06 -0300

    Annotation example

--- src/main/java/br/edu/fatec/main/service/UserService.java
+++ src/main/java/br/edu/fatec/main/service/UserService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.edu.fatec.main.model.AnnotationModel;
 import br.edu.fatec.main.model.UserModel;
+import br.edu.fatec.main.persistency.AnnotationRepository;
 import br.edu.fatec.main.persistency.UserRepository;
 import br.edu.fatec.main.transients.User;
 
@@ -18,11 +20,28 @@ public class UserService {
 	private UserRepository userRepository;
 	
 	@Autowired
+	private AnnotationRepository annotationRepository;
+	
+	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
 	public UserModel save(UserModel user) {
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		return userRepository.save(user);
+		user = userRepository.save(user);
+		
+		AnnotationModel annotation = new AnnotationModel();
+		annotation.setUser(user);
+		annotation.setTitle("Anotação de exemplo 1");
+		annotation.setCafeteria("Cafeteria visitada");
+		annotation.setBarista("Barista da cafeteria");
+		annotation.setCafe("Nome do café degustado");
+		annotation.setComplemento("Complemento utilizado");
+		annotation.setDescricao("Breve descrição de sua anotação");
+		annotation.setMetodoPreparo("Método de preparo utilizado");
+		annotation.setHarmonizacao("Harmonização utilizada");
+		annotationRepository.save(annotation);
+		
+		return user;
 	}
 
 	public void delete(UserModel user) {
